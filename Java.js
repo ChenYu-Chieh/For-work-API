d2e925c1bff021cbf9fb918e51c916e9f8811843
ChenYu-Chieh <cloud550exc@gmail.com>
2020-02-22 23:45:00 +0800

    API 串接測試 success

--- Java.js
+++ Java.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
+	
+	//console.log('helloworld');
+	$('#header').find('div').css({
+		'background-color':'lightblue',
+		'color':'white',
+		'font-size':'24px'
+	});
 
-	$('#header').css({'background-color':'lightblue','color':'white','font-size':'24px'});
+	$.ajax({
+  		url: 'https://randomuser.me/api/',
+  		dataType: 'json',
+  		success: function(data){
+    	console.log('這是抓下來的資料: ',data);
 
-	$ajax()
+    	var email = data.results[0].email;
+    	var gender = data.results[0].gender;
+
+    		//console.log(email);
+    		$('#test').append(email);
+    		//console.log(gender);
+ 		
+    	
+	}});
+	
 
 });
