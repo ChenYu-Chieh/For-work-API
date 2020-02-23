6e36467d8bd602ebd1cc841706471d0372202fca
ChenYu-Chieh <cloud550exc@gmail.com>
2020-02-23 16:01:39 +0800

    test 1

--- Java.js
+++ Java.js
@@ -9,10 +9,11 @@ $(document).ready(function(){
 
 	$.ajax({
 		
-		url:'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&maxResults=12&key=[AIzaSyBfWFKkPVwJcR5LrA-g5_tuwMdIBmP_LzU]' +
+		url:'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&maxResults=12&key=[AIzaSyBfWFKkPVwJcR5LrA-g5_tuwMdIBmP_LzU] HTTP/1.1' +
+
 		'Authorization: Bearer [YOUR_ACCESS_TOKEN]' +
 		'Accept: application/json' ,
-
+		
 		dataType: 'json',
   		success: function(data){
     	console.log('這是抓下來的資料: ',data);
