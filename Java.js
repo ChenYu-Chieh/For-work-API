c84ae1cf74f89754aa4a5383442c3284c353950f
ChenYu-Chieh <cloud550exc@gmail.com>
2020-02-23 22:16:52 +0800

    without () and [] inside url

--- Java.js
+++ Java.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
 	$.ajax({
 		
-		url:'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&chart=mostPopular&maxResults=3&key=[AIzaSyBfWFKkPVwJcR5LrA-g5_tuwMdIBmP_LzU]' ,
+		url:'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&chart=mostPopular&maxResults=3&key=AIzaSyBfWFKkPVwJcR5LrA-g5_tuwMdIBmP_LzU',
 		type:'Get',
 		dataType: 'json',
   		success: function(data){
