5f5c6090666b5d2a0f65afda737e74762c587f65
ChenYu-Chieh <cloud550exc@gmail.com>
2020-02-24 20:18:33 +0800

    Day 19 working on API Data useing focus on jQuery Selector

--- Java.js
+++ Java.js
@@ -22,10 +22,11 @@ $(document).ready(function(){
     	// 	console.log(email);
     	// 	$('#test').append(email);
     	// 	console.log(gender);
-    	var img1, img2, img3, img4, img5;
-    	var img1 = data.Object.items[0].snippet.thumbnails.standard;
-    	$('#img1').attr(img1);
- 		}
+    	var title1 = data.items[0].snippet.title;
+    	console.log(title1);
+ 		$("section:first div h3").text(title1);
+		}
+
 
 	});
 	
