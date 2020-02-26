296c2c22e15588c556b703ec262308ff4a3bd247
ChenYu-Chieh <cloud550exc@gmail.com>
2020-02-26 17:15:53 +0800

    Day 21 迴圈的語法還是不熟...

--- Java.js
+++ Java.js
@@ -25,21 +25,67 @@ $(document).ready(function(){
     	// 	$('#test').append(email);
     	// 	console.log(gender);
     	
-    	var dataItems = data.items;
-    	
+    		var dataItems = data.items;
+    		[i] = Array();
+    		
     		for( i = 0; i < dataItems.length; i++){
-	    		var myVedio = $('#vedio');
-	    		var myTimeCode = $('#timeCode');
-	    		var myTitle = $('#title');
-	    		var myViewCount = $('#viewCount');
-	    		var myPublishedAt = $('publishedAt');
+    		
+	    		//data.items[i] = i;
+	    		//var timeCode = (data.items[i].contentDetails.duration);
+				alert(data.items[i].contentDetails.duration);
+				//$('.timeCode').text(dataItems[i].contentDetails.duration);
+				
+				// $('.timeCode').text(data.items[0].contentDetails.duration);
+				// $('.timeCode').text(data.items[1].contentDetails.duration);
+				// $('.timeCode').text(data.items[2].contentDetails.duration);
+				// $('.timeCode').text(data.items[3].contentDetails.duration);
+				// $('.timeCode').text(data.items[4].contentDetails.duration);
+				// $('.timeCode').text(data.items[5].contentDetails.duration);
+				// $('.timeCode').text(data.items[6].contentDetails.duration);
+				// $('.timeCode').text(data.items[7].contentDetails.duration);
+				// $('.timeCode').text(data.items[8].contentDetails.duration);
+				// $('.timeCode').text(data.items[9].contentDetails.duration);
+				// $('.timeCode').text(data.items[10].contentDetails.duration);
+				// $('.timeCode').text(data.items[11].contentDetails.duration);
+				// $('.timeCode').text(data.items[12].contentDetails.duration);
+			
+			};
+			for (i = 0; i < dataItems.length; i++){
+				
+				document.getElementByClass('timeCode').innerHTML = (data.items[i].contentDetails.duration);
+			};//為什麼這裡不能這樣寫?
 
-	    		myVedio.val(data.items[i].id);
-	    		myTimeCode.text('時長: ' + data.items[i].contentDetails.duration);
-	    		myTitle.text(data.items[i].snippet.title);
-	    		myViewCount.text('觀看人次: '+ data.items[i].statistics.viewCount);
-	    		myPublishedAt.text(data.items[i].snippet.publishedAt);
-    		}
+    			// v.text([1]);
+    			// v[2];
+    			// v[3];
+    			// v[4];
+    			// v[5];
+    			// v[6];
+    			// v[7];
+    			// v[8];
+    			// v[9];
+    			// v[10];
+    			// v[11];
+    			// v[12];
+
+    		// 	v.each(function(){
+    			
+    		// 	$(this).text('時長: ' + data.items[i].contentDetails.duration)
+
+  				// });
+
+	    		//var myVedio = $('#vedio');
+	    		//var myTimeCode = $('#timeCode');
+	    		//var myTitle = $('#title');
+	    		//var myViewCount = $('#viewCount');
+	    		//var myPublishedAt = $('publishedAt');
+
+	    		// myVedio.val(data.items[i].id);
+	    		// myTimeCode.text('時長: ' + data.items[i].contentDetails.duration);
+	    		// myTitle.text(data.items[i].snippet.title);
+	    		// myViewCount.text('觀看人次: '+ data.items[i].statistics.viewCount);
+	    		// myPublishedAt.text(data.items[i].snippet.publishedAt);
+    		
     	
     	//var title1 = data.items[0].snippet.title;
     	//console.log(title1);
